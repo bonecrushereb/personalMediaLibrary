33ec512b52bc4dcc2532874ef9d7d5e0568f44dd
bonecrushereb <bonecrushereb@gmail.com>
2018-07-09 23:24:16 -0700

    work on detail page

--- inc/functions.php
+++ inc/functions.php
@@ -10,13 +10,10 @@
   }
 
   function arrayCategory($catalog, $category) {
-    if ($category == null) {
-      return array_keys($catalog);
-    }
     $output = array();
 
     foreach ($catalog as $id => $item) {
-      if(strtolower($category) == strtolower($item['category'])) {
+      if(strtolower($category == null OR $category) == strtolower($item['category'])) {
         $sort = $item['title'];
         $sort = ltrim($sort, 'The ');
         $sort = ltrim($sort, 'A ');
