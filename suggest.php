f36b976faedcb689ffa989ededa7e4a490b38987
bonecrushereb <bonecrushereb@gmail.com>
2018-07-12 21:42:03 -0700

    merged process.php and thanks.php into suggest.php

--- suggest.php
+++ suggest.php
@@ -1,16 +1,41 @@
 <?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $details = $_POST['details'];
+
+
+  echo "<pre>";
+  $email_body = "";
+  $email_body .= "Name: " . $name . "\n";
+  $email_body .= "Email: " . $email . "\n";
+  $email_body .= "Details: " . $details . "\n";
+  echo $email_body;
+  echo "</pre>";
+
+
+  header('location:suggest.php?status=thanks');
+}
+
 $pageTitle = 'Suggest a Media Item';
 $section = 'suggest';
 
 include('inc/header.php'); 
-
 ?>
 
 <div class="section page">
   <div class="wrapper">
     <h1>Suggest a Media Item</h1>
+    <?php 
+      if (isset($_GET['status']) && $_GET['status'] == 'thanks') {
+          echo "<p>Thanks for the email. I will check out your suggestion shortly!</p>";
+      } else {
+
+    ?>
     <p>If something is missing, let me know! Send me an email.</p>
-    <form method="post" action="process.php">
+    <form method="post" action="suggest.php">
       <table>
         <tr>
           <th>
@@ -39,6 +64,7 @@ include('inc/header.php');
       </table>
       <input type="submit" value="send">
     </form>
+  <?php } ?>
   </div>
 </div>
 
