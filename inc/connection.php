f871c585c024b25f879db46e9dfc37dedad2ed24
bonecrushereb <bonecrushereb@gmail.com>
2018-07-22 22:24:22 -0700

    add more expections for error handling

--- inc/connection.php
+++ inc/connection.php
@@ -2,10 +2,10 @@
 
   try{  
     $dsn = "mysql:host=localhost;dbname=database;port=8889"; $username = "root"; $password = "root"; 
-    $db = new PDO($dsn, $username, $password); 
-    var_dump($db); 
+    $db = new PDO($dsn, $username, $password);
+    $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(Exception $e) {
-    echo $e;
+    echo $e->getMessage();
     exit;
   }
 
